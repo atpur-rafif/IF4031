5b7745fc9d7c928a0cf503b372964a3fec833ece
atpur-rafif <muhammadatpurrafif@gmail.com>
2025-12-27 06:24:21 +0000

    feat: list complaint

--- modules/complaint.js
+++ modules/complaint.js
@@ -5,9 +5,7 @@ import { pool, transaction } from "./database.js";
 import { authMiddleware } from "./auth.js"
 import { fileUpload } from "./body.js";
 
-const visibilityEnum = ['public', 'private'];
-const statusEnum = ['open', 'closed', 'in-progress'];
-
+const entriesPerPage = 100
 export const complaintRouter = express.Router()
 
 export const createComplaintSchema = z.object({
@@ -19,6 +17,45 @@ export const createComplaintSchema = z.object({
 });
 
 complaintRouter.get("/complaint", authMiddleware([]), async (req, res) => {
+	const role = req.user?.role
+	const select = sql`
+		SELECT complaint_id, user_id, users.name AS user, complaints.department_id, private, anonymous, title, status
+		FROM complaints JOIN users USING(user_id)`
+
+	const roleFilter =
+		role === "admin" ? sql`` :
+		role === "department" ? sql` WHERE private = false OR department_id = ${req.user.departmentId}` :
+		role === "user" ? sql` WHERE private = false OR user_id = ${req.user.userId}` :
+			sql` WHERE private = false`
+
+	const counter = sql`SELECT COUNT(1) FROM complaints`
+	const { rows: [{ count }] } = await pool.query(counter.append(roleFilter))
+	const maxPage = Math.max(Math.ceil(count / entriesPerPage), 1)
+	const page = Math.max(req.query.page ?? 0, 0)
+	console.log(page, maxPage, count)
+
+	if(page >= maxPage) throw {
+		status: 500,
+		message: "Invalid page"
+	}
+
+	const start = page * entriesPerPage;
+	const end = start + entriesPerPage;
+	const pagination = sql` OFFSET ${start} LIMIT ${end}`
+
+	const { rows } = await pool.query(select.append(roleFilter).append(pagination))
+	const data = rows.map(entry => {
+		if(!entry.anonymous) {
+			delete entry.user_id
+			delete entry.user
+		}
+
+		return entry
+	})
+	return res.json({ data, maxPage })
+})
+
+complaintRouter.post("/complaint", authMiddleware(["user"]),
 	fileUpload.single("attachment"), async (req, res) => {
 	if(req.user.role !== "user") throw {
 		status: 401,
