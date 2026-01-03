301f13585ca1b9abb79e65243b75de2ea467e8c3
atpur-rafif <muhammadatpurrafif@gmail.com>
2026-01-03 12:36:11 +0000

    feat: show complaints frontend

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -1,1 +1,17 @@
-console.log("Hello, world!")
+const main = document.querySelector("#main")
+
+async function showComplaints(){
+	const res = await fetch("/api/complaint")
+	const { data: complaints } = await res.json()
+	console.log(complaints)
+
+	main.innerHTML = `<div id="complaint">
+		${complaints.map(complaint => `<div>
+			<a href="/complaint/${complaint.complaint_id}">${complaint.title} (${complaint.status})</a>
+			<p>From: ${complaint.user ?? "Anonymous"}</p>
+			<p>To: ${complaint.department}</p>
+		</div>`).join("")}
+	</div>`
+}
+
+showComplaints()
